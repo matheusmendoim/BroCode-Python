fcf529fbef35de4c6c7952a61936657135e4a295
matheusmendoim <matheusinho.mfsm@gmail.com>
2023-02-22 17:11:03 -0300

    Class 2 - Variables

--- 2. variables.py
+++ 2. variables.py
@@ -6,12 +6,46 @@
 # x + 5 = 15
 # 10 + 5 = 15
 
-#strings: series of characteres
+
+#strings: series of characters
 
 first_name = "Matheus"
 last_name = "Mendoim"
 full_name = first_name + " " + last_name
 
-print(type(first_name)+"\n")
-
+print(type(first_name))
 print("Hello", full_name)
+
+
+#integers: contains whole integers numbers
+
+age = 21
+age += 1
+
+print(age)
+print(type(age))
+print("Your age is: "+str(age))
+
+
+#float: data type that stores decimal numbers
+
+height = 175.03
+
+print(height)
+print(type(height))
+print("Your height is "+str(height)+" cm")
+
+
+#boolean: variable that only store true of false
+
+human = True
+
+print(human)
+print(type(human))
+print("Are you a human? "+str(human))
+
+
+#str
+#int
+#float
+#bool
