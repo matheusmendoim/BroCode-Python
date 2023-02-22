368a2949bbb5f195837d542655d230765549a26b
matheusmendoim <matheusinho.mfsm@gmail.com>
2023-02-22 16:58:41 -0300

    Class 2 - Variables

---
+++ 2. variables.py
@@ -0,0 +1,17 @@
+# Variables are a container for a value
+# And behaves like the value that it contains
+
+# seems like algebra.
+# x = 10
+# x + 5 = 15
+# 10 + 5 = 15
+
+#strings: series of characteres
+
+first_name = "Matheus"
+last_name = "Mendoim"
+full_name = first_name + " " + last_name
+
+print(type(first_name)+"\n")
+
+print("Hello", full_name)
